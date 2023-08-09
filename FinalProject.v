f1e2c99824901c6c31edcaa7ae295bb700677ec9
EthanM827 <ethanmcfetridge@gmail.com>
2023-08-09 09:41:56 -0400

    add temperature input

--- FinalProject.v
+++ FinalProject.v
@@ -1,6 +1,10 @@
-module FinalProject (input clk, pwr, key0, key1, sw0, sw1, sw2, sw3, sw4, sw5, output wire [0:6] hex0, hex1, hex2, hex3, hex4, hex5);
+module FinalProject (input clk, pwr, key0, key1, sw5, sw4, sw3, sw2, sw1, sw0, output wire [0:6] hex0, hex1, hex2, hex3, hex4, hex5);
+	parameter default_temp = 300;
+	parameter max_temp = 500;
+	parameter min_temp = 65;
+	
 	reg new_clk;
-	parameter MAX_COUNT = 25000000;
+	parameter MAX_COUNT = 25000000; 
 	reg [25:0] count = 0;
 	always @ (posedge clk) begin
 		if (count <= MAX_COUNT) begin
@@ -11,8 +15,65 @@ module FinalProject (input clk, pwr, key0, key1, sw0, sw1, sw2, sw3, sw4, sw5, o
 			new_clk = ~new_clk;
 		end
 	end
+	
+	reg key0_old, key1_old;
+	reg key0_edge, key1_edge;
+	always @ (posedge clk) begin
+		key0_old <= key0;
+		key0_edge <= ~(~key0 && key0_old);
+		
+		key1_old <= key1;
+		key1_edge <= ~(~key1 && key1_old);
+	end
+		
+	
 	wire [12:0] current_time;
+	wire [9:0] current_temp;
+	integer target_temp = 300;
+	reg tempInputDone, timeInputDone;
 	timeclk timeclk(clk, current_time);
-	ovenDisplay display(pwr, 0, 0, current_time, hex0, hex1, hex2, hex3, hex4, hex5);
+	ovenDisplay display(pwr, tempInputDone, timeInputDone, 65, target_temp, current_time, 0, hex0, hex1, hex2, hex3, hex4, hex5);
+	
+	integer tempChange, timeChange;
+	always @ (posedge clk) begin
+		if (pwr) begin
+			// continue looping to get target temp until both key0 and key1 pressed
+			if(~(key0 == 0 && key1 == 0)) begin
+				case ({sw5, sw4, sw3, sw2, sw1, sw0})
+					5'b00001: tempChange = 5;
+					5'b00010: tempChange = 10;
+					5'b00100: tempChange = 25;
+					5'b01000: tempChange = 50;
+					5'b10000: tempChange = 100;
+					// only modify value if 1 switch toggled
+					default: tempChange = 0;
+				endcase
+				// increase
+				if (~key0_edge && key1_edge) begin
+					// if new temp would be higher than minimum oven temp, cap it
+					if ((target_temp + tempChange) >= max_temp) begin
+						target_temp = max_temp;
+					end else begin
+						target_temp = target_temp + tempChange;
+					end
+				end
+				// decrease
+				if (~key1_edge && key0_edge) begin
+					// if new temp would be lower than minimum oven temp, cap it
+					if ((target_temp - tempChange) <= min_temp) begin
+						target_temp = min_temp;
+					end else begin
+						target_temp = target_temp - tempChange;
+					end
+				end
+			end else begin
+				tempInputDone = 1;
+			end
+		end else begin
+			target_temp = default_temp;
+			timeInputDone = 0;
+			tempInputDone = 0;
+		end
+	end
 
 endmodule
--- ovenDisplay.v
+++ ovenDisplay.v
@@ -1,4 +1,4 @@
-module ovenDisplay(input power, current_temp, target_temp, input [12:0] current_time, output reg [0:6] hex0, output reg [0:6] hex1, output reg [0:6] hex2, output reg [0:6] hex3, output reg [0:6] hex4, output reg [0:6] hex5);
+module ovenDisplay(input power, tempInputDone, timeInputDone, input [9:0] current_temp, input [9:0] target_temp, input [12:0] current_time, input [12:0] target_time, output reg [0:6] hex0, output reg [0:6] hex1, output reg [0:6] hex2, output reg [0:6] hex3, output reg [0:6] hex4, output reg [0:6] hex5);
 	reg [6:0] hex_1 = 7'b1001111; //1
 	reg [6:0] hex_2 = 7'b0010010; //2
 	reg [6:0] hex_3 = 7'b0000110; //3
@@ -11,30 +11,135 @@ module ovenDisplay(input power, current_temp, target_temp, input [12:0] current_
 	reg [6:0] hex_0 = 7'b0000001; //0
 	reg [6:0] hex_blank = 7'b1111111;
 	
-	integer minutes, minutes_ones_place, minutes_tens_place, seconds, seconds_ones_place, seconds_tens_place;
+	integer current_time_minutes, current_time_minutes_ones_place, current_time_minutes_tens_place, current_time_seconds, current_time_seconds_ones_place, current_time_seconds_tens_place;
+	integer target_time_minutes, target_time_minutes_ones_place, target_time_minutes_tens_place, target_time_seconds, target_time_seconds_ones_place, target_time_seconds_tens_place;
+	integer current_temp_ones_place, current_temp_tens_place, current_temp_hundreds_place;
+	integer target_temp_ones_place, target_temp_tens_place, target_temp_hundreds_place;
 	
 	always @ (*) begin
-		minutes = (current_time - (current_time % 60)) / 60;
-		minutes_ones_place = minutes % 10;
-		minutes_tens_place = (minutes - minutes_ones_place) / 10;
+		// current_time values
+		current_time_minutes = (current_time - (current_time % 60)) / 60;
+		current_time_minutes_ones_place = current_time_minutes % 10;
+		current_time_minutes_tens_place = (current_time_minutes - current_time_minutes_ones_place) / 10;
 
-		seconds = current_time - (minutes * 60);
-		seconds_ones_place = seconds % 10;
-		seconds_tens_place = (seconds - seconds_ones_place) / 10;
+		current_time_seconds = current_time - (current_time_minutes * 60);
+		current_time_seconds_ones_place = current_time_seconds % 10;
+		current_time_seconds_tens_place = (current_time_seconds - current_time_seconds_ones_place) / 10;
+		
+		// target_time values
+		target_time_minutes = (target_time - (target_time % 60)) / 60;
+		target_time_minutes_ones_place = target_time_minutes % 10;
+		target_time_minutes_tens_place = (target_time_minutes - target_time_minutes_ones_place) / 10;
+
+		target_time_seconds = target_time - (target_time_minutes * 60);
+		target_time_seconds_ones_place = target_time_seconds % 10;
+		target_time_seconds_tens_place = (target_time_seconds - target_time_seconds_ones_place) / 10;
+		
+		// current_temp values
+		current_temp_ones_place = current_temp % 10;
+		current_temp_tens_place = ((current_temp % 100) - current_temp_ones_place) / 10;
+		current_temp_hundreds_place = (current_temp - (current_temp_tens_place * 10) - current_temp_ones_place) / 100;
+		
+		// target_temp values
+		target_temp_ones_place = target_temp % 10;
+		target_temp_tens_place = ((target_temp % 100) - target_temp_ones_place) / 10;
+		target_temp_hundreds_place = (target_temp - (target_temp_tens_place * 10) - target_temp_ones_place) / 100;
 	end
 		
 	always@(*)begin
 		if (power) begin
-			hex0 = hex_blank;
-			hex1 = hex_blank;
-			hex2 = hex_blank;
-			hex3 = hex_blank;
-			hex4 = hex_blank;
-			hex5 = hex_blank;
+			if (~tempInputDone) begin
+				case (target_temp_ones_place)
+					1: hex0 = hex_1;
+					2: hex0 = hex_2;
+					3: hex0 = hex_3;
+					4: hex0 = hex_4;
+					5: hex0 = hex_5;
+					6: hex0 = hex_6;
+					7: hex0 = hex_7;
+					8: hex0 = hex_8;
+					9: hex0 = hex_9;
+					0: hex0 = hex_0;
+				endcase
+				case (target_temp_tens_place)
+					1: hex1 = hex_1;
+					2: hex1 = hex_2;
+					3: hex1 = hex_3;
+					4: hex1 = hex_4;
+					5: hex1 = hex_5;
+					6: hex1 = hex_6;
+					7: hex1 = hex_7;
+					8: hex1 = hex_8;
+					9: hex1 = hex_9;
+					0: hex1 = hex_0;
+				endcase
+				case (target_temp_hundreds_place)
+					1: hex2 = hex_1;
+					2: hex2 = hex_2;
+					3: hex2 = hex_3;
+					4: hex2 = hex_4;
+					5: hex2 = hex_5;
+					6: hex2 = hex_6;
+					7: hex2 = hex_7;
+					8: hex2 = hex_8;
+					9: hex2 = hex_9;
+					0: hex2 = hex_0;
+				endcase
+					
+				hex3 = hex_blank;
+				hex4 = hex_blank;
+				hex5 = hex_blank;
+			end else begin
+				if (~timeInputDone) begin
+					hex2 = 7'b1111110;
+					case (target_time_seconds_ones_place)
+						1: hex0 = hex_1;
+						2: hex0 = hex_2;
+						3: hex0 = hex_3;
+						4: hex0 = hex_4;
+						5: hex0 = hex_5;
+						6: hex0 = hex_6;
+						7: hex0 = hex_7;
+						8: hex0 = hex_8;
+						9: hex0 = hex_9;
+						0: hex0 = hex_0;
+					endcase
+					case (target_time_seconds_tens_place)
+						1: hex1 = hex_1;
+						2: hex1 = hex_2;
+						3: hex1 = hex_3;
+						4: hex1 = hex_4;
+						5: hex1 = hex_5;
+						0: hex1 = hex_0;
+					endcase
+					case (target_time_minutes_ones_place)
+						1: hex3 = hex_1;
+						2: hex3 = hex_2;
+						3: hex3 = hex_3;
+						4: hex3 = hex_4;
+						5: hex3 = hex_5;
+						6: hex3 = hex_6;
+						7: hex3 = hex_7;
+						8: hex3 = hex_8;
+						9: hex3 = hex_9;
+						0: hex3 = hex_0;
+					endcase
+					case (target_time_minutes_tens_place)
+						1: hex4 = hex_1;
+						2: hex4 = hex_2;
+						3: hex4 = hex_3;
+						4: hex4 = hex_4;
+						5: hex4 = hex_5;
+						0: hex4 = hex_0;
+					endcase
+				end else begin
+				
+				end
+			end
 		end else begin
 			hex5 = hex_blank;
 			hex2 = 7'b1111110;
-			case(minutes_ones_place)
+			case(current_time_minutes_ones_place)
 				1: hex3 = hex_1;
 				2: hex3 = hex_2;
 				3: hex3 = hex_3;
@@ -46,7 +151,7 @@ module ovenDisplay(input power, current_temp, target_temp, input [12:0] current_
 				9: hex3 = hex_9;
 				0: hex3 = hex_0;
 			endcase
-			case(minutes_tens_place)
+			case(current_time_minutes_tens_place)
 				1: hex4 = hex_1;
 				2: hex4 = hex_2;
 				3: hex4 = hex_3;
@@ -54,7 +159,7 @@ module ovenDisplay(input power, current_temp, target_temp, input [12:0] current_
 				5: hex4 = hex_5;
 				0: hex4 = hex_0;
 			endcase
-			case(seconds_ones_place)
+			case(current_time_seconds_ones_place)
 				1: hex0 = hex_1;
 				2: hex0 = hex_2;
 				3: hex0 = hex_3;
@@ -66,7 +171,7 @@ module ovenDisplay(input power, current_temp, target_temp, input [12:0] current_
 				9: hex0 = hex_9;
 				0: hex0 = hex_0;
 			endcase
-			case(seconds_tens_place)
+			case(current_time_seconds_tens_place)
 				1: hex1 = hex_1;
 				2: hex1 = hex_2;
 				3: hex1 = hex_3;
