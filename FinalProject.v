18060a6a87ea768e92e00c61e5b19308aebd7b13
Ryuta Ide <ider@wit.edu>
2023-08-09 10:40:42 -0400

    update

--- FinalProject.v
+++ FinalProject.v
@@ -2,6 +2,7 @@ module FinalProject (input clk, pwr, key0, key1, sw5, sw4, sw3, sw2, sw1, sw0, o
 	parameter default_temp = 300;
 	parameter max_temp = 500;
 	parameter min_temp = 65;
+	parameter max_time = 1800;	
 	
 	reg new_clk;
 	parameter MAX_COUNT = 25000000; 
@@ -33,13 +34,13 @@ module FinalProject (input clk, pwr, key0, key1, sw5, sw4, sw3, sw2, sw1, sw0, o
 	integer target_time = 0;
 	reg tempInputDone, timeInputDone;
 	timeclk timeclk(clk, current_time);
-	ovenDisplay display(pwr, tempInputDone, timeInputDone, 65, target_temp, current_time, 0, hex0, hex1, hex2, hex3, hex4, hex5);
+	ovenDisplay display(pwr, tempInputDone, timeInputDone, 65, target_temp, current_time, target_time, hex0, hex1, hex2, hex3, hex4, hex5);
 	
 	integer tempChange, timeChange;
 	always @ (posedge clk) begin
 		if (pwr) begin
 			// continue looping to get target temp until both key0 and key1 pressed
-			if(~(key0 == 0 && key1 == 0)) begin
+			if(~(key0 == 0 && key1 == 0)&&~tempInputDone) begin
 				case ({sw5, sw4, sw3, sw2, sw1, sw0})
 					5'b00001: tempChange = 5;
 					5'b00010: tempChange = 10;
@@ -67,9 +68,43 @@ module FinalProject (input clk, pwr, key0, key1, sw5, sw4, sw3, sw2, sw1, sw0, o
 						target_temp = target_temp - tempChange;
 					end
 				end
+
 			end else begin
+				
 				tempInputDone = 1;
+				if(~(key0 == 0 && key1 == 0)&&~tempInputDone) begin
+					case ({sw5, sw4, sw3, sw2, sw1, sw0})
+						5'b00001: timeChange = 5;
+						5'b00010: timeChange = 10;
+						5'b00100: timeChange = 30;
+						5'b01000: timeChange = 60;
+						5'b10000: timeChange = 300;
+						// only modify value if 1 switch toggled
+						default: timeChange = 0;
+					endcase
+					// increase
+					if (~key0_edge && key1_edge) begin
+						// if new temp would be higher than minimum oven temp, cap it
+						if ((target_time + timeChange) >= max_time) begin
+							target_time = max_time;
+						end else begin
+							target_time = target_time + timeChange;
+						end
+					end
+					// decrease
+					if (~key1_edge && key0_edge) begin
+						// if new temp would be lower than minimum oven temp, cap it
+						if ((target_time - timeChange) <= 0) begin
+							target_time = 0;
+						end else begin
+							target_time = target_time - timeChange;
+						end
+					end
+				end else begin
+					timeInputDone = 1;
+				end
 			end
+
 		end else begin
 			target_temp = default_temp;
 			target_time = 0;
